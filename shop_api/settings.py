244ad5c5b75d28a34c1df838191a22ceac0f850c
Baibol <musaevbaibol@gmail.com>
2026-06-19 22:03:47 +0600

    HW-7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-import dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv.load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
